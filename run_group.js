d4e20e3cf8522edc5c6f3341f07144f1d779e4cc
Dustin Hendrix <hendrixd@coyote.csusb.edu>
2013-01-29 09:58:06 -0800

    updated to the new version of the assignment, complete

--- run_group.js
+++ run_group.js
@@ -1,43 +1,67 @@
+// Return a random integer between 0 and 1000
 function rand() {
   return Math.floor(Math.random() * 1000);
 }
 
-function f1() {
+function f1(cb) {
   console.log('f1 starts');
-  setTimeout(function() {
-    console.log('f1 ends');
-  }, rand());
+  setTimeout(cb, rand());
 }
 
-function f2() {
+function f2(cb) {
   console.log('f2 starts');
-  setTimeout(function() {
-    console.log('f2 ends');
-  }, rand());
+  setTimeout(cb, rand());
 }
 
-function f3() {
+function f3(cb) {
   console.log('f3 starts');
-  setTimeout(function() {
-    console.log('f3 ends');
-  }, rand());
+  setTimeout(cb, rand());
 }
 
 function onEnd() {
-  console.log('f1, f2 and f3 have ended.');
+  console.log('onEnd called');
 }
 
 /* 
   PROBLEM:
   
-  Figure out a way to run f1, f2 and f3 to completion before
-  running onEnd.  Do this without modifying f1, f2, f3, and onEnd.
-  
-  Replace the following code with something that works.
-  Do not modify anything above this comment.
-*/
+  Figure out a way to run f1, f2 and f3 in 
+  parallel and to completion before running onEnd.
 
-f1();
-f2();
-f3();
-setTimeout(function () { onEnd(); }, rand() * 4);
+  The program should complete as quickly as possible.
+
+  Do not modify anything above this comment;
+  do not modify f1, f2, f3, and onEnd.
+  
+  Run this program with the following command:
+
+     node run_group.js
+
+  Notice that the onEnd function runs before the
+  other functions complete.
+
+  Replace the code following this comment with 
+  something that works.
+line 45 */
+var count = 3;
+f1(function() { 
+	console.log('f1 completed'); 
+	count--;
+	if(count === 0) onEnd();
+});
+f2(function() { 
+	console.log('f2 completed'); 
+	count--;
+	if(count === 0) onEnd();
+});
+f3(function() { 
+	console.log('f3 completed'); 
+	count--;
+	if(count === 0) onEnd();
+});
+
+
+
+
+
+
